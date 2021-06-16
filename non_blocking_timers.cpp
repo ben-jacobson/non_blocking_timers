958d1f0b11e4d110fab3794fe1594106a5b8620e
Ben <ben_jacobson@live.com>
2021-06-17 08:00:28 +1000

    refactored in linked list

--- non_blocking_timers.cpp
+++ non_blocking_timers.cpp
@@ -1,81 +1,61 @@
 #include "non_blocking_timers.h"
 
-non_blocking_timer *get_last_timer_in_list(non_blocking_timer *timer_ptr) {
-    non_blocking_timer *last_timer_in_list = timer_ptr->first_timer_in_list; 
 
-    while (last_timer_in_list->next_timer_in_list != nullptr) {
-        last_timer_in_list = last_timer_in_list->next_timer_in_list;
-    }
-    return last_timer_in_list;
+void add_new_generic_timer(non_blocking_timer *non_blocking_timer_ptr, unsigned int _end_after, bool _repeating) {
+    // initialize the timer 
+    non_blocking_timer_ptr->finished = false;      
+    non_blocking_timer_ptr->repeating = _repeating;      
+    non_blocking_timer_ptr->end_after = _end_after;
+    non_blocking_timer_ptr->start_time = millis();
+    last_timer_in_list->next_timer_in_list = non_blocking_timer_ptr; // the user has created a new timer, link it in the list. 
+    last_timer_in_list = non_blocking_timer_ptr; // update the last timer in the list
 }
 
-
-void add_new_timer(non_blocking_timer *timer_array, unsigned int _end_after, bool _repeating) {
-    int first_available = scan_for_available_timers(timer_array);
-
-    if (first_available != -1)  {
-        timer_array[first_available].finished = false;      
-        timer_array[first_available].repeating = _repeating;      
-        timer_array[first_available].end_after = _end_after;
-        timer_array[first_available].start_time = millis();
-    }
+void init_digitalWrite_timer(non_blocking_timer *non_blocking_timer_ptr, unsigned int _end_after, uint8_t _ulPin, uint8_t _ulVal) {
+    pinMode(_ulPin, OUTPUT); // the pin must be an digital output
+    non_blocking_timer_ptr->ulPin = _ulPin;
+    non_blocking_timer_ptr->ulVal = _ulVal;
+    add_new_generic_timer(non_blocking_timer_ptr, _end_after, false);
 }
 
-void init_digitalWrite_timer(non_blocking_timer timer_array[], unsigned int _end_after, uint32_t _ulPin, uint32_t _ulVal) {
-    int first_available = scan_for_available_timers(timer_array);
-
-    pinMode(_ulPin, OUTPUT);
-    
-    if (first_available != -1) {        
-        timer_array[first_available].ulPin = _ulPin;
-        timer_array[first_available].ulVal = _ulVal;
-        timer_array[first_available].event_callback = nullptr; // disable the callback fn to avoid any confusion
-        add_new_timer(&timer_array[first_available], _end_after, false);
-    }
+void init_digitalWrite_oscillator(non_blocking_timer *non_blocking_timer_ptr, unsigned int _cycle_duration, uint32_t _ulPin, uint32_t _start_ulVal) {
+    pinMode(_ulPin, OUTPUT); // the pin must be an digital output
+    non_blocking_timer_ptr->ulPin = _ulPin;
+    non_blocking_timer_ptr->ulVal = _start_ulVal;
+    add_new_generic_timer(non_blocking_timer_ptr, _cycle_duration, true);  
 }
 
-void init_digitalWrite_oscillator(non_blocking_timer timer_array[], unsigned int _cycle_duration, uint32_t _ulPin, uint32_t _start_ulVal) {
-    int first_available = scan_for_available_timers(timer_array);
-    
-    pinMode(_ulPin, OUTPUT);
-
-    if (first_available != -1) {        
-        timer_array[first_available].ulPin = _ulPin;
-        timer_array[first_available].ulVal = _start_ulVal;
-        timer_array[first_available].event_callback = nullptr; // disable the callback fn to avoid any confusion
-        add_new_timer(&timer_array[first_available], _cycle_duration, true);
-    }    
+void init_function_timer(non_blocking_timer *non_blocking_timer_ptr, unsigned int _end_after, callback_function event_callback, bool _repeating) {
+    non_blocking_timer_ptr->event_callback = event_callback;
+    add_new_generic_timer(non_blocking_timer_ptr, _end_after, _repeating);
 }
 
-void init_function_timer(non_blocking_timer timer_array[], unsigned int _end_after, callback_function event_callback, bool _repeating = false) {
-    uint8_t first_available = scan_for_available_timers(timer_array);                
-    timer_array[first_available].event_callback = event_callback;
-    add_new_timer(&timer_array[first_available], _end_after, _repeating);
-}
-
-void update_timers(non_blocking_timer timer_array[]) {
+void update_timers(void) {
     unsigned long int current_time = millis();
+    non_blocking_timer *timer_to_update = &static_timer_first_in_list;
 
-    for (uint32_t i = 0; i < non_blocking_timer::MAX_TIMERS; i++) {
-        if (timer_array[i].finished == false && current_time - timer_array[i].start_time >= timer_array[i].end_after) {
+    while (timer_to_update->next_timer_in_list != nullptr) {
+        timer_to_update = timer_to_update->next_timer_in_list;
+
+        if (timer_to_update->finished == false && current_time - timer_to_update->start_time >= timer_to_update->end_after) {
             // timer has expired
-            if (timer_array[i].event_callback != nullptr) { // execute the callback if that's how the timer is set up
-                timer_array[i].event_callback();
+            if (timer_to_update->event_callback != nullptr) { // execute the callback if that's how the timer is set up
+                timer_to_update->event_callback();
             }
             else {
-                digitalWrite(timer_array[i].ulPin, timer_array[i].ulVal); // change our pin value if that's how the timer is set up
+                digitalWrite(timer_to_update->ulPin, timer_to_update->ulVal); // change our pin value if that's how the timer is set up
 
-                if (timer_array[i].repeating == true) {
-                    timer_array[i].ulVal = !timer_array[i].ulVal; // flip the HIGH/LOW if a pin.
+                if (timer_to_update->repeating == true) {
+                    timer_to_update->ulVal = !timer_to_update->ulVal; // flip the HIGH/LOW if a pin.
                 }
             }
 
-            if (timer_array[i].repeating == false) {
-                timer_array[i].finished = true; // allow this to be used once again. 
+            if (timer_to_update->repeating == false) {
+                timer_to_update->finished = true; // allow this to be used once again. 
             }
             else {
-                timer_array[i].start_time = millis(); // reset timer                
+                timer_to_update->start_time = millis(); // reset timer                
             }
-        }
+        }      
     }
 }
--- non_blocking_timers.h
+++ non_blocking_timers.h
@@ -4,22 +4,25 @@
 #include <Arduino.h>
 
 // not recommended for anything less than 5ms
-typedef void (*callback_function)(void);    // syntactic sugar for function pointer, allows for cleaner code when taking a function pointer as an argument. 
+
+typedef void (*callback_function)(void);    // syntactic sugar for a function pointer, allows for cleaner code when taking a function pointer as an argument. 
 
 struct non_blocking_timer {
     unsigned long int start_time, end_after;
-    void (*event_callback)(void); // for use with timers needing to call a callback function, at the moment only works with functions that return void types
-    uint8_t ulPin, ulVal; // for use with timers needing to alter pin values
     bool finished = true; 
     bool repeating = false; 
-    non_blocking_timer *first_timer_in_list, *next_timer_in_list = nullptr;     // we used linked lists to create multiple pointers. The first timer is the first in the list, and every object points to the next timer in the list. 
+    non_blocking_timer *next_timer_in_list = nullptr;     // we use a linked list to make timers easy to create in arrays, or standalone, independant of scope
+    uint8_t ulPin, ulVal; // for use with timers needing to alter pin values
+    void (*event_callback)(void) = nullptr; // for use with timers needing to call a callback function, at the moment only works with functions that return void types
 }; 
 
-non_blocking_timer *get_last_timer_in_list(non_blocking_timer *timer);
-void add_new_timer(non_blocking_timer timer_array[], unsigned int _end_after, bool _repeating);
-void init_digitalWrite_timer(non_blocking_timer timer_array[], unsigned int _end_after, uint8_t _ulPin, uint8_t _ulVal);
-void init_digitalWrite_oscillator(non_blocking_timer timer_array[], unsigned int _cycle_duration, uint32_t _ulPin, uint32_t _start_ulVal);
-void init_function_timer(non_blocking_timer timer_array[], unsigned int _end_after, callback_function event_callback, bool _repeating);
-void update_timers(non_blocking_timer timer_array[]);
+void add_new_generic_timer(non_blocking_timer *non_blocking_timer_ptr, unsigned int _end_after, bool _repeating);
+void init_digitalWrite_timer(non_blocking_timer *non_blocking_timer_ptr, unsigned int _end_after, uint8_t _ulPin, uint8_t _ulVal);
+void init_digitalWrite_oscillator(non_blocking_timer *non_blocking_timer_ptr, unsigned int _cycle_duration, uint32_t _ulPin, uint32_t _start_ulVal);
+void init_function_timer(non_blocking_timer *non_blocking_timer_ptr, unsigned int _end_after, callback_function event_callback, bool _repeating);
+void update_timers(void);
+
+non_blocking_timer static_timer_first_in_list; // the first item in the list is an unused timer which acts as the static base
+non_blocking_timer *last_timer_in_list = &static_timer_first_in_list;
 
 #endif
