91598a5ef4b808475309dc18d79c82119b7ee08a
Ben <ben_jacobson@live.com>
2021-06-17 08:34:52 +1000

    working through some bugs in my linked list algorithms

--- non_blocking_timers.cpp
+++ non_blocking_timers.cpp
@@ -1,5 +1,7 @@
 #include "non_blocking_timers.h"
 
+non_blocking_timer static_timer_first_in_list; // the first item in the list is an unused timer which acts as the static base
+non_blocking_timer *last_timer_in_list = &static_timer_first_in_list;   // last timer in linked list, this will continually update as needed.
 
 void add_new_generic_timer(non_blocking_timer *non_blocking_timer_ptr, unsigned int _end_after, bool _repeating) {
     // initialize the timer 
@@ -7,8 +9,17 @@ void add_new_generic_timer(non_blocking_timer *non_blocking_timer_ptr, unsigned 
     non_blocking_timer_ptr->repeating = _repeating;      
     non_blocking_timer_ptr->end_after = _end_after;
     non_blocking_timer_ptr->start_time = millis();
-    last_timer_in_list->next_timer_in_list = non_blocking_timer_ptr; // the user has created a new timer, link it in the list. 
-    last_timer_in_list = non_blocking_timer_ptr; // update the last timer in the list
+
+    last_timer_in_list->next_timer_in_list = non_blocking_timer_ptr; // update the last timer to point to the new timer (even if its the first)
+    non_blocking_timer_ptr->prev_timer_in_list = last_timer_in_list; // update the new timers prev timer
+    last_timer_in_list = non_blocking_timer_ptr; // then update the last timer in the list
+}
+
+void delete_timer(non_blocking_timer *non_blocking_timer_ptr) {
+    non_blocking_timer *next_timer = non_blocking_timer_ptr->next_timer_in_list;
+    non_blocking_timer *prev_timer = non_blocking_timer_ptr->prev_timer_in_list;
+    prev_timer->next_timer_in_list = next_timer; // link the previous item to the next item, thus removing the current item. 
+    next_timer->prev_timer_in_list = prev_timer;    // if this happens to the first item in linked lust, it will copy the static base next timer which is nullptr
 }
 
 void init_digitalWrite_timer(non_blocking_timer *non_blocking_timer_ptr, unsigned int _end_after, uint8_t _ulPin, uint8_t _ulVal) {
@@ -51,7 +62,8 @@ void update_timers(void) {
             }
 
             if (timer_to_update->repeating == false) {
-                timer_to_update->finished = true; // allow this to be used once again. 
+                timer_to_update->finished = true; // allow this to be used once again.
+                delete_timer(timer_to_update);
             }
             else {
                 timer_to_update->start_time = millis(); // reset timer                
--- non_blocking_timers.h
+++ non_blocking_timers.h
@@ -3,7 +3,7 @@
 
 #include <Arduino.h>
 
-// not recommended for anything less than 5ms
+// This library is not recommended for use with anything less than 5ms.
 
 typedef void (*callback_function)(void);    // syntactic sugar for a function pointer, allows for cleaner code when taking a function pointer as an argument. 
 
@@ -11,18 +11,16 @@ struct non_blocking_timer {
     unsigned long int start_time, end_after;
     bool finished = true; 
     bool repeating = false; 
-    non_blocking_timer *next_timer_in_list = nullptr;     // we use a linked list to make timers easy to create in arrays, or standalone, independant of scope
+    non_blocking_timer *prev_timer_in_list = nullptr, *next_timer_in_list = nullptr;     // we use a linked list to make timers easy to create in arrays, or standalone, independant of scope
     uint8_t ulPin, ulVal; // for use with timers needing to alter pin values
     void (*event_callback)(void) = nullptr; // for use with timers needing to call a callback function, at the moment only works with functions that return void types
 }; 
 
 void add_new_generic_timer(non_blocking_timer *non_blocking_timer_ptr, unsigned int _end_after, bool _repeating);
+void delete_timer(non_blocking_timer *non_blocking_timer_ptr);
 void init_digitalWrite_timer(non_blocking_timer *non_blocking_timer_ptr, unsigned int _end_after, uint8_t _ulPin, uint8_t _ulVal);
 void init_digitalWrite_oscillator(non_blocking_timer *non_blocking_timer_ptr, unsigned int _cycle_duration, uint32_t _ulPin, uint32_t _start_ulVal);
 void init_function_timer(non_blocking_timer *non_blocking_timer_ptr, unsigned int _end_after, callback_function event_callback, bool _repeating);
 void update_timers(void);
-
-non_blocking_timer static_timer_first_in_list; // the first item in the list is an unused timer which acts as the static base
-non_blocking_timer *last_timer_in_list = &static_timer_first_in_list;
 
 #endif
